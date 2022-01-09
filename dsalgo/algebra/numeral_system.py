5813b118744eac53c3db56a88c760a32fb0c736c
kagemeka <kagemeka1@gmail.com>
2022-01-09 17:57:17 +0900

    docs: add documents for numeral system

--- dsalgo/algebra/numeral_system.py
+++ dsalgo/algebra/numeral_system.py
@@ -3,8 +3,23 @@ import typing
 
 def base_convert_to_ten(
     base: int,
-    digits: typing.Iterable[int],
+    digits: typing.List[int],
 ) -> int:
+    """Convert digits to base ten integer.
+
+    Args:
+        base (int): original base.
+        digits (typing.List[int]): digits.
+
+    Returns:
+        int: base ten integer.
+
+
+    Examples:
+        >>> digits = [0, 1, 1]
+        >>> base_convert_to_ten(2, digits)
+        6
+    """
     assert abs(base) >= 2
     p = 1
     n = 0
@@ -18,6 +33,19 @@ def base_convert_from_ten(
     base: int,
     n: int,
 ) -> typing.List[int]:
+    """Convert base ten integer to arbitral base digits.
+
+    Args:
+        base (int): target base.
+        n (int): base ten integer.
+
+    Returns:
+        typing.List[int]: result digits.
+
+    Examples:
+        >>> base_convert_from_ten(-2, 10)
+        [0, 1, 1, 1, 1]
+    """
     assert abs(base) >= 2
     if n == 0:
         return [0]
@@ -36,7 +64,23 @@ def base_convert(
     b1: int,
     digits: typing.List[int],
 ) -> typing.List[int]:
+    """Convert digits base from b0 to b1.
+
+    Args:
+        b0 (int): original base.
+        b1 (int): target base.
+        digits (typing.List[int]): original digits.
+
+    Returns:
+        typing.List[int]: target digits.
+    """
     return base_convert_from_ten(
         b1,
         base_convert_to_ten(b0, digits),
     )
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod(verbose=True)
