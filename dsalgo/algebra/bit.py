9c6788020f8f1c1c9eb0741ccb773c15927576ac
kagemeka <kagemeka1@gmail.com>
2022-01-10 10:15:39 +0900

    docs: add documents for bit

--- dsalgo/algebra/bit.py
+++ dsalgo/algebra/bit.py
@@ -2,10 +2,6 @@ import functools
 import sys
 import typing
 
-import pytest
-
-sys.setrecursionlimit(1 << 20)
-
 
 def bit_length_naive(n: int) -> int:
     length = 0
@@ -22,10 +18,29 @@ def bit_length_table(n: int) -> list[int]:
 
 
 def popcount_naive(n: int) -> int:
-    r"""Popcount naive.
+    r"""Popcount Naive.
 
-    O(\log{N})
+    Args:
+        n (int): an unsigned integer.
+
+    Returns:
+        int: popcount := count of active binary bits.
+
+    Complexity:
+        time: O(\log{N})
+        space: O(1)
+
+    Examples:
+        >>> popcount_naive(0b1010)
+        2
+        >>> popcount_naive(0b1100100)
+        3
+        >>> popcount_naive(-1)
+        Traceback (most recent call last):
+        ...
+        AssertionError: n must be unsinged integer.
     """
+    assert n >= 0, 'n must be unsinged integer.'
     cnt = 0
     while n:
         cnt += n & 1
@@ -35,15 +50,44 @@ def popcount_naive(n: int) -> int:
 
 @functools.lru_cache(maxsize=None)
 def popcount_cached(n: int) -> int:
+    r"""Popcount Cashed.
+
+    Args:
+        n (int): an unsigned integer.
+
+    Returns:
+        int: popcount := count of active binary bits.
+
+    Complexity:
+        time: O(1) amortized.
+            best: O(1)
+            worst: O(\log{n})
+        space: O(max(n))
+
+    Examples:
+        >>> popcount_naive(0b1010)
+        2
+        >>> popcount_naive(0b1100100)
+        3
+        >>> popcount_naive(-1)
+        Traceback (most recent call last):
+        ...
+        AssertionError: n must be unsinged integer.
+    """
+    assert n >= 0, 'n must be unsinged integer.'
     if n == 0:
         return 0
     return popcount_cached(n >> 1) + (n & 1)
 
 
 def popcount_table(n: int) -> typing.List[int]:
-    r"""Popcount table.
+    """Popcount table.
 
-    O(N)
+    Args:
+        n (int): an unsigned integer.
+
+    Returns:
+        typing.List[int]: popcount table.
     """
     cnt = [0] * n
     for i in range(n):
@@ -52,9 +96,25 @@ def popcount_table(n: int) -> typing.List[int]:
 
 
 def popcount(n: int) -> int:
-    r"""Popcount.
+    """Popcount.
 
-    O(1)
+    Args:
+        n (int): an 64-bit signed or unsigned integer.
+
+    Returns:
+        int: popcount := count of active binary bits.
+
+    Complexity:
+        time: O(1)
+        space: O(1)
+
+    Examples:
+        >>> popcount(0b1010)
+        2
+        >>> popcount(0b1100100)
+        3
+        >>> popcount(-1)
+        64
     """
     n -= (n >> 1) & 0x5555555555555555
     n = (n & 0x3333333333333333) + ((n >> 2) & 0x3333333333333333)
@@ -65,6 +125,7 @@ def popcount(n: int) -> int:
     return n & 0x0000007F
 
 
-@pytest.fixture
-def test_popcount() -> typing.NoReturn:
-    assert popcount(1) == 1
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod(verbose=True)
