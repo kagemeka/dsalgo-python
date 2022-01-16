9c7edadc94c728e2ab06aebf83258fd1609ae503
kagemeka <kagemeka1@gmail.com>
2022-01-16 19:04:59 +0900

    implement tsp

--- src/dsalgo/graph_theory/traveling_salesman.py
+++ src/dsalgo/graph_theory/traveling_salesman.py
@@ -1,46 +1,38 @@
 # TODO cut below
 import typing
 
-from ..graph import Graph
 
-
-class TSP:
-    def __call__(
-        self,
-        g: Graph,
-        src: int,
-    ) -> int:
-        g = self.__graph_to_matrix(g)
-        return self.from_matrix(g, src)
-
-    def __graph_to_matrix(self, g: Graph) -> list[list[int]]:
-        n = g.size
-        dist = [[0] * n for _ in range(n)]
+def traveling_salesperson(
+    graph: typing.List[typing.List[typing.Optional[int]]],
+    src: int,
+) -> typing.Optional[int]:
+    n = len(graph)
+    assert all(len(edges) == n for edges in graph)
+    assert 0 <= src < n
+    dist: typing.List[typing.List[typing.Optional[int]]] = [
+        [None] * n for _ in range(1 << n)
+    ]
+    dist[1 << src][src] = 0
+    for s in range(1 << n):
         for i in range(n):
-            for e in g.edges[i]:
-                dist[i][e.to] = e.weight
-        return dist
-
-    def from_matrix(
-        self,
-        g: list[list[int]],
-        src: int,
-    ) -> int:
-        n = len(g)
-        assert len(g[0]) == n
-        inf = float("inf")
-        dist = [[inf] * n for _ in range(1 << n)]
-        dist[1 << src][src] = 0
-        for s in range(1 << n):
-            for i in range(n):
-                if ~s >> i & 1:
+            if ~s >> i & 1:
+                continue
+            t = s & ~(1 << i)
+            for j in range(n):
+                if ~t >> j & 1 or dist[t][j] is None or graph[j][i] is None:
                     continue
-                for j in range(n):
-                    if s >> j & 1:
-                        continue
-                    u = s | 1 << j
-                    dist[u][j] = min(
-                        dist[u][j],
-                        dist[s][i] + g[i][j],
-                    )
-        return min(dist[-1][i] + g[i][src] for i in range(n))
+                d = dist[t][j] + graph[j][i]
+                if dist[s][i] is None or d < dist[s][i]:
+                    dist[s][i] = d
+    
+    mn: typing.Optional[int] = None
+    for i in range(n):
+        if i == src or dist[-1][i] is None or graph[i][src] is None:
+            continue
+        d = dist[-1][i] + graph[i][src]
+        if mn is None or d < mn:
+            mn = d
+    return mn
+    
+    
+     
