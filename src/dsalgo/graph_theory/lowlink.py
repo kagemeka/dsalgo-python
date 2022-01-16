2c84fe687c626fdb329b7e4d3a1e1c32cc23a018
kagemeka <kagemeka1@gmail.com>
2022-01-16 12:47:43 +0900

    implement articulation points

--- src/dsalgo/graph_theory/lowlink.py
+++ src/dsalgo/graph_theory/lowlink.py
@@ -1,11 +1,16 @@
-
-
 import typing 
 
-def compute_lowlink(
-    graph: typing.List[typing.List[typing.Tuple[int, int]]],
+
+def lowlink_undirected(
+    n: int,
+    edges: typing.List[typing.Tuple[int, int]],
 ) -> typing.Tuple[typing.List[int], typing.List[int]]:
-    n = len(graph)
+    graph: typing.List[typing.List[typing.Tuple[int, int]]] = [
+        [] for _ in range(n)
+    ]
+    for i, (u, v) in enumerate(edges):
+        graph[u].append((v, i))
+        graph[v].append((u, i))
     order = [-1] * n
     lowlink = [-1] * n
     ord = 0
@@ -29,86 +34,85 @@ def compute_lowlink(
     return order, lowlink
 
 
-def find_bridges(
+def lowlink_directed():
+    ...
+
+
+def bridges_lowlink(
     n: int,
     edges: typing.List[typing.Tuple[int, int]],
-) -> typing.List[typing.Tuple[int, int]]:
+) -> typing.List[int]:
+    # bridge is defined on undirected graph.
+    # https://cp-algorithms.com/graph/bridge-searching.html
+    order, lowlink = lowlink_undirected(n, edges)
+    bridge_ids: typing.List[int] = []
+    for i, (u, v) in enumerate(edges):
+        if order[u] > order[v]:
+            u, v = v, u
+        if lowlink[v] > order[u]:
+            bridge_ids.append(i)
+    return bridge_ids
+
+
+def strong_bridges():
+    ...
+
+
+def articulation_points_lowlink(
+    n: int,
+    edges: typing.List[typing.Tuple[int, int]],
+) -> typing.List[int]:
+    # articulation point is defined on undirected graph.
+    # https://cp-algorithms.com/graph/cutpoints.html
     graph: typing.List[typing.List[typing.Tuple[int, int]]] = [
         [] for _ in range(n)
     ]
     for i, (u, v) in enumerate(edges):
         graph[u].append((v, i))
         graph[v].append((u, i))
-    order, lowlink = compute_lowlink(graph)
-    bridge_ids: typing.List[typing.Tuple[int, int]] = []
-    for i, (u, v) in enumerate(edges):
-        if order[u] > order[v]:
-            u, v = v, u
-        if lowlink[v] > order[u]:
-            bridge_ids.append(i)
-            # bridges.append((u, v) if u < v else (v, u))
-    return bridge_ids
+    order = [-1] * n
+    lowlink = [-1] * n
+    ord = 0
+    articulation_points: typing.List[int] = []
+
+    def dfs(u: int, edge_id_to_u: int) -> None:
+        nonlocal ord
+        order[u] = lowlink[u] = ord
+        ord += 1
+        num_childs = 0
+        is_articulation = False
+        for v, edge_id in graph[u]:
+            if edge_id == edge_id_to_u:
+                continue
+            if order[v] != -1:
+                lowlink[u] = min(lowlink[u], order[v])
+                continue
+            num_childs += 1
+            dfs(v, edge_id)
+            lowlink[u] = min(lowlink[u], lowlink[v])
+            is_articulation |= edge_id_to_u != -1 and lowlink[v] >= order[u]
+        is_articulation |= edge_id_to_u == -1 and num_childs >= 2 
+        if is_articulation:
+            articulation_points.append(u)
+    
+    for i in range(n):
+        if order[i] == -1:
+            dfs(i, -1)
+    print(order, lowlink)
+    return sorted(articulation_points)
 
 
-edges = [(0, 1), (0, 3), (1, 2), (1, 4), (2, 3)]
-n = 5
-print(find_bridges(n, edges))
+def strong_articulation_points():
+    ...
 
 
-# pub fn bridges(n: usize, g: &Vec<(usize, usize)>) -> Vec<(usize, usize)> {
-#     let mut t = vec![vec![]; n];
-#     for (u, v) in g {
-#         t[*u].push(*v);
-#         t[*v].push(*u);
-#     }
-#     let (order, low) = lowlink(&t);
-#     let mut bridges = Vec::new();
-#     for (u, v) in g {
-#         let mut u = *u;
-#         let mut v = *v;
-#         if order[u] > order[v] { std::mem::swap(&mut u, &mut v); }
-#         if low[v] <= order[u] { continue; }
-#         if u > v { std::mem::swap(&mut u, &mut v); } 
-#         bridges.push((u, v)); 
-
-#     }
-#     bridges.sort();
-#     bridges
-# }
 
 
-# pub fn articulation_points(g: &Vec<Vec<usize>>) -> Vec<usize> {
-#     let n = g.len();
-#     let mut articulation_points = Vec::new();
-#     let (order, low) = lowlink(g);
-#     let mut visited = vec![false; n];
-#     fn dfs(
-#         g: &Vec<Vec<usize>>, 
-#         u: usize, 
-#         parent: usize, 
-#         visited: &mut Vec<bool>,
-#         order: &Vec<usize>, 
-#         low: &Vec<usize>, 
-#         points: &mut Vec<usize>,
-#     ) {
-#         let n = g.len();
-#         visited[u] = true;
-#         let mut cnt = 0u32;
-#         let mut is_articulation = false;
-#         for v in g[u].iter().map(|x| *x) {
-#             if visited[v] { continue; }
-#             cnt += 1;
-#             dfs(g, v, u, visited, order, low, points);
-#             if parent == n || low[v] < order[u] { continue; }
-#             is_articulation = true;
-#         }
-#         if parent == n && cnt >= 2 { is_articulation = true; }
-#         if is_articulation { points.push(u); }
-#     }
-#     for i in 0..n {
-#         if visited[i] { continue; }
-#         dfs(g, i, n, &mut visited, &order, &low, &mut articulation_points);
-#     }
-#     articulation_points.sort();
-#     articulation_points
-# } 
+# edges = [(0, 1), (0, 3), (1, 2), (1, 4), (2, 3)]
+# n = 5
+# print(bridges_lowlink(n, edges))
+
+
+
+# print(articulation_points_lowlink(n, edges))
+# print(edges)
