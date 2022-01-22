deeb03e197e070eba8155f2e287e9c04bf7ee7a9
kagemeka <kagemeka1@gmail.com>
2022-01-22 17:58:14 +0900

    implement pivot_tree_set

--- src/dsalgo/graph_theory/tree/pivot_tree/pivot_tree_recurse.py
+++ src/dsalgo/graph_theory/tree/pivot_tree/pivot_tree_recurse.py
@@ -49,7 +49,6 @@ def insert(root: Node[V], key: int, value: V) -> None:
         if root.left is None:
             p = root.pivot
             root.left = Node(p - (p & -p) // 2, lo_key, lo_value)
-            # return
         else:
             insert(root.left, lo_key, lo_value)
     else:
@@ -57,7 +56,6 @@ def insert(root: Node[V], key: int, value: V) -> None:
         if root.right is None:
             p = root.pivot
             root.right = Node(p + (p & -p) // 2, hi_key, hi_value)
-            # return
         else:
             insert(root.right, hi_key, hi_value)
     root.size += 1
@@ -136,57 +134,3 @@ def upper_bound(root: typing.Optional[Node[V]], key: int) -> int:
     if root.key <= key:
         return _get_size(root.left) + 1 + upper_bound(root.right, key)
     return upper_bound(root.left, key)
-
-
-root: typing.Optional[Node] = new_tree_root(4, 7, 2)
-insert(root, 3, 6)
-print(find(root, 7))
-root = remove(root, 7)
-for i in range(5, 10):
-    insert(root, i, 1)
-
-print(lower_bound(root, 3))
-print(root)
-print(get_kth_node(root, 5))
-
-
-# class PivotTree(typing.Generic[V]):
-#     __root: Node[V]
-#     __max_size: int
-#     __size: int
-
-#     def __init__(self, max_size: int) -> None:
-#         self.__max_size = max_size
-#         self.__root = new_tree(max_size)
-#         self.__size = 0
-
-#     @property
-#     def max_size(self) -> int:
-#         return self.__max_size
-
-#     def __len__(self) -> int:
-#         return self.__size
-
-#     def __contains__(self, key: int) -> bool:
-#         assert 0 <= key < self.max_size
-#         return find(self.__root, key + 1) is not None
-
-#     def insert(self, key: int, value: typing.Optional[V] = None) -> None:
-#         assert 0 <= key < self.max_size
-#         if key in self:
-#             return
-#         insert(self.__root, key + 1, value)
-#         self.__size += 1
-
-#     def remove(self, key: int) -> None:
-#         assert 0 <= key < self.max_size
-#         if key not in self:
-#             return
-#         remove(self.__root, key + 1)
-#         self.__size -= 1
-
-
-# tree = PivotTree[int](max_size=10)
-# tree.insert(2)
-# print(len(tree))
-# print(2 in tree)
---
+++ src/dsalgo/graph_theory/tree/pivot_tree/pivot_tree_recurse_test.py
@@ -0,0 +1,10 @@
+# root: typing.Optional[Node] = new_tree_root(4, 7, 2)
+# insert(root, 3, 6)
+# print(find(root, 7))
+# root = remove(root, 7)
+# for i in range(5, 10):
+#     insert(root, i, 1)
+
+# print(lower_bound(root, 3))
+# print(root)
+# print(get_kth_node(root, 5))
