c765a3e1d6a86a01765800cac1a290d2c3106df0
kagemeka <kagemeka1@gmail.com>
2022-01-26 00:59:15 +0900

    implement euler totient lpf

--- src/dsalgo/number_theory/euler_totient/euler_totient.py
+++ src/dsalgo/number_theory/euler_totient/euler_totient.py
@@ -1,15 +1,12 @@
 import typing
 
+from dsalgo.number_theory.prime_number.find_prime_factors import (
+    find_prime_factors,
+)
+
 
 def euler_totient(n: int) -> int:
-    count, p = n, 1
-    while p * p < n:
-        p += 1
-        if n % p:
-            continue
+    count = n
+    for p in find_prime_factors(n):
         count = count // p * (p - 1)
-        while n % p == 0:
-            n //= p
-    if n > 1:
-        count = count // n * (n - 1)
     return count
--- src/dsalgo/number_theory/euler_totient/euler_totient_least_prime_factor.py
+++
@@ -1,8 +0,0 @@
-import typing
-
-
-def euler_totient_least_prime_factor(
-    max_value: int,
-    least_prime_factor: typing.List[int],
-) -> int:
-    ...
