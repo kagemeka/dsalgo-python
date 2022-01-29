5b4a0165527139c8682ac582fef7932c535de99a
kagemeka <kagemeka1@gmail.com>
2022-01-29 11:39:28 +0900

    implement miller_rabin

--- src/dsalgo/number_theory/prime_number/primality/fermat.py
+++ src/dsalgo/number_theory/prime_number/primality/fermat.py
@@ -1,19 +1,12 @@
 import math
 import random
-import typing
-
-# https://en.wikipedia.org/wiki/Fermat_primality_test
-# https://ja.wikipedia.org/wiki/%E3%83%95%E3%82%A7%E3%83%AB%E3%83%9E%E3%83%BC%E3%81%AE%E5%B0%8F%E5%AE%9A%E7%90%86#%E3%83%95%E3%82%A7%E3%83%AB%E3%83%9E%E3%83%BC%E3%83%86%E3%82%B9%E3%83%88
-# https://qiita.com/srtk86/items/609737d50c9ef5f5dc59
-
-# https://ja.wikipedia.org/wiki/%E3%82%AB%E3%83%BC%E3%83%9E%E3%82%A4%E3%82%B1%E3%83%AB%E6%95%B0
 
 
 def fermat_test(n: int, check_times: int = 100) -> bool:
     """Fermat Primality Test.
 
     Args:
-        n (int): a nutural number.
+        n (int): a natural number.
         check_times (int, optional):
             how many times it should be checked?
             (for detail https://en.wikipedia.org/wiki/Fermat_primality_test)
@@ -52,9 +45,10 @@ def fermat_test(n: int, check_times: int = 100) -> bool:
         base = random.randint(2, n - 1)
         if base in checked_bases:
             continue
-        if n_is_composite(base):
+        if n_is_composite(base):  # the base is called witness.
             return False
         checked_bases.add(base)
 
     # might be pseudo prime like Carmichael number.
+    # if not prime actually, each checked base is called liar.
     return True
--- src/dsalgo/number_theory/prime_number/primality/miller_rabin.py
+++ src/dsalgo/number_theory/prime_number/primality/miller_rabin.py
@@ -1,12 +1,49 @@
+import random
 import typing
 
 
-def miller_rabin(n: int) -> bool:
-    ...
+def is_composite_trivial(n: int) -> bool:
+    assert n >= 1
+    return n == 1 or n & 1 == 0 and n != 2
 
 
-#   https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test#Testing_against_small_sets_of_bases
-def miller_rabin_32(n: int) -> bool:
+def is_composite(n: int, base: int) -> bool:
+    assert n >= 3
+    r, d = 0, n - 1
+    while d & 1 == 0:
+        r += 1
+        d >>= 1
+    # n - 1 = d2^r
+    x = pow(base, d, n)
+    for _ in range(r):
+        y = x * x % n
+        if y == 1:
+            return x != 1 and x != n - 1
+        x = y
+    return True
+
+
+def _miller_rabin_fixed_bases(n: int, bases: typing.List[int]) -> bool:
+    assert n >= 1
+    if is_composite_trivial(n):
+        return False
+    if n == 2:
+        return True
+    for base in bases:
+        if is_composite(n, base):
+            return False
+    return True
+
+
+def miller_rabin_test(n: int, check_times: int = 20) -> bool:
+    assert n >= 1
+    if n == 1:
+        return False
+    bases = list(set(random.randint(1, n - 1) for _ in range(check_times)))
+    return _miller_rabin_fixed_bases(n, bases)
+
+
+def miller_rabin_test_32(n: int) -> bool:
     #   https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test#Testing_against_small_sets_of_bases
     BASES: typing.Final[
         typing.Tuple[
@@ -15,9 +52,37 @@ def miller_rabin_32(n: int) -> bool:
             typing.Literal[61],
         ]
     ] = (2, 7, 61)
-    return True
-    ...
+    return _miller_rabin_fixed_bases(n, list(BASES))
 
 
-def miller_rabin_64(n: int) -> bool:
-    return True
+def miller_rabin_test_64(n: int) -> bool:
+    #   https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test#Testing_against_small_sets_of_bases
+    BASES: typing.Final[typing.List[int]] = [
+        2,
+        3,
+        5,
+        7,
+        11,
+        13,
+        17,
+        19,
+        23,
+        29,
+        31,
+        37,
+    ]
+    return _miller_rabin_fixed_bases(n, BASES)
+
+
+def miller_rabin_test_64_v2(n: int) -> bool:
+    # http://miller-rabin.appspot.com/
+    BASES: typing.Final[typing.List[int]] = [
+        2,
+        325,
+        9375,
+        28178,
+        450775,
+        9780504,
+        1795265022,
+    ]
+    return _miller_rabin_fixed_bases(n, BASES)
