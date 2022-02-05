0f649ee1fdbd6b69dd2126802f77ff161bc3d1e8
kagemeka <kagemeka1@gmail.com>
2022-02-05 14:23:15 +0000

    docs: union find

--- src/dsalgo/graph_theory/union_find/union_find.py
+++ src/dsalgo/graph_theory/union_find/union_find.py
@@ -2,19 +2,51 @@ import typing
 
 
 class UnionFind:
+    """UnionFind DataStructure."""
+
     def __init__(self, size: int) -> None:
+        """Initialize with size.
+
+        Args:
+            size (int): count of nodes in UnionFind Forest.
+        """
         self.__data = [-1] * size
 
     def __len__(self) -> int:
+        """Length of data.
+
+        Returns:
+            int: equal to the size of nodes.
+        """
         return len(self.__data)
 
     def find_root(self, node: int) -> int:
+        """Find root node of the component in which given node contained.
+
+        Args:
+            node (int): target node.
+
+        Returns:
+            int: root node.
+        """
+        assert 0 <= node < len(self)
         if self.__data[node] < 0:
             return node
         self.__data[node] = self.find_root(self.__data[node])
         return self.__data[node]
 
     def unite(self, node_u: int, node_v: int) -> None:
+        """Unite two components.
+
+        Args:
+            node_u (int): a node.
+            node_v (int): another node.
+
+        Note:
+            If node_u and node_v are contained in the same components,
+            do nothing and return early.
+        """
+        assert 0 <= node_u < len(self) and 0 <= node_v < len(self)
         node_u, node_v = self.find_root(node_u), self.find_root(node_v)
         if node_u == node_v:
             return
@@ -24,38 +56,27 @@ class UnionFind:
         self.__data[node_v] = node_u
 
     def size(self, node: int) -> int:
+        """Size of the component of given node.
+
+        Args:
+            node (int): an arbitrary node.
+
+        Returns:
+            int: [description]
+        """
+        assert 0 <= node < len(self)
         return -self.__data[self.find_root(node)]
 
 
-# class UnionFind:
-#     def __init__(self, n: int) -> None:
-#         self.__data = [-1] * n
-
-#     def __len__(self) -> int:
-#         return len(self.__data)
-
-#     def find(self, u: int) -> int:
-#         d = self.__data
-#         if d[u] < 0:
-#             return u
-#         d[u] = self.find(d[u])
-#         return d[u]
-
-#     def unite(self, u: int, v: int) -> None:
-#         u, v = self.find(u), self.find(v)
-#         if u == v:
-#             return
-#         d = self.__data
-#         if d[u] > d[v]:
-#             u, v = v, u
-#         d[u] += d[v]
-#         d[v] = u
-
-#     def size(self, u: int) -> int:
-#         return -self.__data[self.find(u)]
-
-
 def get_labels(uf: UnionFind) -> typing.List[int]:
+    """Get labels of nodes.
+
+    Args:
+        uf (UnionFind): an union find instance.
+
+    Returns:
+        typing.List[int]: labels.
+    """
     n = len(uf)
     labels = [-1] * n
     label = 0
@@ -66,3 +87,9 @@ def get_labels(uf: UnionFind) -> typing.List[int]:
             label += 1
         labels[i] = labels[root]
     return labels
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod(verbose=True)
