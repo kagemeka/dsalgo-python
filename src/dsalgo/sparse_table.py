65165ebcc4cb2c3cfa09ead4e290eeeed100d230
kagemeka <kagemeka1@gmail.com>
2022-02-18 02:35:13 +0000

    sparse table

--- src/dsalgo/sparse_table.py
+++ src/dsalgo/sparse_table.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import typing
 
 from dsalgo.algebra.abstract.abstract_structure import Semigroup
@@ -37,6 +38,148 @@ def sparse_table(
             return data[0][left]
         k = bit_length[right - 1 - left] - 1
         return semigroup.op(data[k][left], data[k][right - (1 << k)])
+
+    return get
+
+
+def sparse_table_int_gcd() -> typing.Callable[[int, int], int]:
+    ...
+
+
+def sparse_table_int_min() -> typing.Callable[[int, int], int]:
+    ...
+
+
+def sparse_table_2d():
+    ...
+
+
+import typing
+
+from dsalgo.algebra.abstract.abstract_structure import Semigroup
+from dsalgo.algebra.bit.bit_length import bit_length_table
+
+S = typing.TypeVar("S")
+
+
+def disjoint_sparse_table(
+    semigroup: Semigroup[S],
+    arr: list[S],
+) -> typing.Callable[[int, int], S]:
+    """Disjoint Sparse Table.
+
+    Args:
+        semigroup (Semigroup[S]): semigroup.
+        arr (list[S]): original array.
+
+    Returns:
+        typing.Callable[[int, int], S]: function to get product [left, right).
+    """
+    n = len(arr)
+    assert n > 0
+    bit_length = bit_length_table(n << 1)
+    k = max(1, bit_length[n - 1])
+    data = [arr.copy()]
+    for i in range(1, k):
+        data.append(arr.copy())
+        for j in range(1 << i, n + 1, 2 << i):
+            for k in range(1, 1 << i):
+                data[i][j - k - 1] = semigroup.op(
+                    data[i][j - k - 1],
+                    data[i][j - k],
+                )
+            for k in range((1 << i) - 1):
+                if j + k + 1 >= n:
+                    break
+                data[i][j + k + 1] = semigroup.op(
+                    data[i][j + k],
+                    data[i][j + k + 1],
+                )
+
+    def get(left: int, right: int) -> S:
+        assert 0 <= left < right <= n
+        if right - left == 1:
+            return data[0][left]
+        k = bit_length[left ^ (right - 1)] - 1
+        return semigroup.op(data[k][left], data[k][right - 1])
+
+    return get
+
+
+
+from dsalgo.algebra.bit.bit_length import bit_length_table
+
+
+def disjoint_sparse_table_int_xor(
+    arr: list[int],
+) -> typing.Callable[[int, int], int]:
+    """Disjoint Sparse Table int-xor.
+
+    Args:
+        arr (list[int]): original array.
+
+    Returns:
+        typing.Callable[[int, int], int]:
+            function to get xor product [left, right).
+    """
+    n = len(arr)
+    assert n > 0
+    bit_length = bit_length_table(n << 1)
+    k = max(1, bit_length[n - 1])
+    data = [arr.copy()]
+    for i in range(1, k):
+        data.append(arr.copy())
+        for j in range(1 << i, n + 1, 2 << i):
+            for k in range(1, 1 << i):
+                data[i][j - k - 1] ^= data[i][j - k]
+            for k in range((1 << i) - 1):
+                if j + k + 1 >= n:
+                    break
+                data[i][j + k + 1] ^= data[i][j + k]
+
+    def get(left: int, right: int) -> int:
+        assert 0 <= left < right <= n
+        if right - left == 1:
+            return data[0][left]
+        k = bit_length[left ^ (right - 1)] - 1
+        return data[k][left] ^ data[k][right - 1]
+
+    return get
+
+
+def disjoint_sparse_table_int_sum(
+    arr: list[int],
+) -> typing.Callable[[int, int], int]:
+    """Disjoint Sparse Table int-sum.
+
+    Args:
+        arr (list[int]): original array.
+
+    Returns:
+        typing.Callable[[int, int], int]:
+            function to get sum [left, right).
+    """
+    n = len(arr)
+    assert n > 0
+    bit_length = bit_length_table(n << 1)
+    k = max(1, bit_length[n - 1])
+    data = [arr.copy()]
+    for i in range(1, k):
+        data.append(arr.copy())
+        for j in range(1 << i, n + 1, 2 << i):
+            for k in range(1, 1 << i):
+                data[i][j - k - 1] += data[i][j - k]
+            for k in range((1 << i) - 1):
+                if j + k + 1 >= n:
+                    break
+                data[i][j + k + 1] += data[i][j + k]
+
+    def get(left: int, right: int) -> int:
+        assert 0 <= left < right <= n
+        if right - left == 1:
+            return data[0][left]
+        k = bit_length[left ^ (right - 1)] - 1
+        return data[k][left] + data[k][right - 1]
 
     return get
 
--- src/dsalgo/sparse_table_test.py
+++ src/dsalgo/sparse_table_test.py
@@ -4,7 +4,7 @@ from dsalgo.algebra.abstract.abstract_structure import Semigroup
 from dsalgo.range_query.sparse_table.sparse_table import sparse_table
 
 
-class Test(unittest.TestCase):
+class TestSparseTable(unittest.TestCase):
     def test_min(self) -> None:
         a = [3, 1, 2, 10, -1]
         semigroup = Semigroup[int](op=min)
@@ -12,6 +12,64 @@ class Test(unittest.TestCase):
         self.assertEqual(get_min(0, 5), -1)
         self.assertEqual(get_min(0, 1), 3)
         self.assertEqual(get_min(0, 3), 1)
+
+
+import operator
+import unittest
+
+from dsalgo.algebra.abstract.abstract_structure import Semigroup
+from dsalgo.range_query.sparse_table.disjoint_sparse_table import (
+    disjoint_sparse_table,
+)
+
+
+class TestDisjointSparseTable(unittest.TestCase):
+    def test_min(self) -> None:
+        a = [3, 1, 2, 10, -1]
+        semigroup = Semigroup[int](op=min)
+        get_min = disjoint_sparse_table(semigroup, a)
+        self.assertEqual(get_min(0, 5), -1)
+        self.assertEqual(get_min(0, 1), 3)
+        self.assertEqual(get_min(0, 3), 1)
+
+    def test_sum(self) -> None:
+        a = [3, 1, 2, 10, -1]
+        semigroup = Semigroup[int](op=operator.add)
+        get_sum = disjoint_sparse_table(semigroup, a)
+        self.assertEqual(get_sum(0, 5), 15)
+        self.assertEqual(get_sum(0, 1), 3)
+        self.assertEqual(get_sum(0, 3), 6)
+
+    def test_xor(self) -> None:
+        a = [3, 1, 2, 10, 0]
+        semigroup = Semigroup[int](op=operator.xor)
+        get_xor = disjoint_sparse_table(semigroup, a)
+        self.assertEqual(get_xor(0, 5), 10)
+        self.assertEqual(get_xor(0, 1), 3)
+        self.assertEqual(get_xor(0, 3), 0)
+
+
+from dsalgo.range_query.sparse_table.disjoint_sparse_table_int_xor import (
+    disjoint_sparse_table_int_xor,
+)
+
+
+class TestDisjointSparseTableIntXor(unittest.TestCase):
+    def test(self) -> None:
+        a = [3, 1, 2, 10, 0]
+        get_xor = disjoint_sparse_table_int_xor(a)
+        self.assertEqual(get_xor(0, 5), 10)
+        self.assertEqual(get_xor(0, 1), 3)
+        self.assertEqual(get_xor(0, 3), 0)
+
+
+class TestDisjointSparseTableIntSum(unittest.TestCase):
+    def test(self) -> None:
+        a = [3, 1, 2, 10, -1]
+        get_sum = disjoint_sparse_table_int_sum(a)
+        self.assertEqual(get_sum(0, 5), 15)
+        self.assertEqual(get_sum(0, 1), 3)
+        self.assertEqual(get_sum(0, 3), 6)
 
 
 if __name__ == "__main__":
