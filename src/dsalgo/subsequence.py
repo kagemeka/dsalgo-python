141bf2e40167aa5186af72e5e27058d4f5127760
kagemeka <kagemeka1@gmail.com>
2022-02-22 06:25:42 +0000

    subsequence

--- src/dsalgo/subsequence.py
+++ src/dsalgo/subsequence.py
@@ -1,27 +1,35 @@
-"""
-DP
-"""
-
 from __future__ import annotations
 
 import typing
 
-T = typing.TypeVar("T")
+from dsalgo.type import T
+
+
+def count_common_subsequences(
+    a: typing.Sequence[T],
+    b: typing.Sequence[T],
+    mod: int | None = None,
+) -> int:
+    n, m = len(a), len(b)
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    dp[0][0] = 1
+    for i in range(n):
+        dp[i + 1][0] = 1
+    for j in range(m):
+        dp[0][j + 1] = 1
+    for i in range(n):
+        for j in range(m):
+            dp[i + 1][j + 1] = dp[i][j + 1] + dp[i + 1][j] - dp[i][j]
+            dp[i + 1][j + 1] += dp[i][j] * (a[i] == b[j])
+            if mod:
+                dp[i + 1][j + 1] %= mod
+    return dp[-1][-1]
 
 
 def longest_common_subsequence(
     a: typing.Sequence[T],
     b: typing.Sequence[T],
 ) -> list[T]:
-    """Longest Common Subsequence.
-
-    Args:
-        a (np.ndarray): first array.
-        b (np.ndarray): second array.
-
-    Returns:
-        np.ndarray: result.
-    """
     n, m = len(a), len(b)
     length = [[0] * (m + 1) for _ in range(n + 1)]
     for i in range(n):
@@ -45,3 +53,7 @@ def longest_common_subsequence(
         i -= 1
         j -= 1
     return lcs[::-1]
+
+
+def count_subsequences(arr: typing.Sequence[int]) -> int:
+    ...
